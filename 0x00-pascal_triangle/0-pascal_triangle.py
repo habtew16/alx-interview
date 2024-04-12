4c6fa5afa33b6dfbb4852145a20b0c9935b4f27c
habtew16 <habtamuwolde96@gmail.com>
2024-04-12 05:32:03 +0000

    pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+0-main
+"""
+
+
+def pascal_triangle(n):
+    """
+    generate pascals trinagle
+    """
+    if n <= 0:
+        return []
+    res = [[1]]
+
+    for i in range(n - 1):
+        tmp = [0] + res[-1] + [0]
+        row = []
+        for j in range(len(res[-1]) + 1):
+            row.append(tmp[j] + tmp[j + 1])
+        res.append(row)
+    return res
